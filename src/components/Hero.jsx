8df65b458823c5fb14554977c31925e095ac4f87
sok-leaphea <leapheas2@gmail.com>
2024-09-23 14:20:22 +0700

    task: hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import '../style/Hero.css'
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1 className="hero-title">Making Commerce Better for Everyone</h1>
+        <p className="hero-description">Shopify is supporting the next generation of entrepreneurs, the world’s biggest brands, and everyone in between</p>
+        <button className="hero-button">Get Started</button>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
